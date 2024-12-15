1983cc11973522ee491efa2df2279eb4fcba841f
ctemme <chris.temme@gmail.com>
2024-12-15 00:10:47 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+# File structure:
+# /your-app-name/
+#   ├── app.py
+#   ├── requirements.txt
+#   └── .gitignore
+
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
+
